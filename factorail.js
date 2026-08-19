f8a8eb4963b4ef3c9f1b259069d9a59c3fad394c
Tharunjinka6 <tharunjinka2155@gmail.com>
2026-08-20 01:09:33 +0530

    factorial

---
+++ factorail.js
@@ -0,0 +1,11 @@
+class Solution {
+    factorial(n) {
+        // code here
+        let facto=1
+        for(let i=1; i<=n; i++){
+            facto=facto*i
+        }
+        return facto
+    }
+}
+
