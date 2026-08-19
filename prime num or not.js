864dc1346270deef42681567da3da22182740bc7
Tharunjinka6 <tharunjinka2155@gmail.com>
2026-08-19 17:44:16 +0530

    prime or not

---
+++ prime num or not.js
@@ -0,0 +1,15 @@
+class Solution {
+    isPrime(n) {
+        // code here
+        if(n<=1){
+            return false;
+        } 
+        for(let i=2; i<n; i++){
+            if(n%i==0){
+                return false;
+                
+            }
+        }
+        return true;
+    }
+}
