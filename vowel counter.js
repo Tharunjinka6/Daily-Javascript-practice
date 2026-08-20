95188359c547f272740d22f38de9e45ddf83167b
Tharunjinka6 <tharunjinka2155@gmail.com>
2026-08-20 18:26:41 +0530

    vowel counter after removing duplicates

--- vowel counter.js
+++ vowel counter.js
@@ -1,6 +1,7 @@
 let n="aabuu";
+let result = [...new Set(n)].join("");
 let count=0;
-for(let element of n){
+for(let element of result){
     if(element=='a' || element=='e' || element=='i' || element=='o' || element=='u' ){
         count=count+1;
     }
